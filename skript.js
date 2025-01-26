26a9968646f0c6d5c29eea59591d01a71e3b4873
RemoteKar <126679513+RemoteKar@users.noreply.github.com>
2025-01-26 19:09:12 +0900

    Add files via upload

--- skript.js
+++ skript.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const menuLinks = document.querySelectorAll('.menu-link'); // 사이드바 링크
+  const menuLinks = document.querySelectorAll('.menu-link'); // 메뉴 링크
   const sections = document.querySelectorAll('.section'); // 모든 섹션
 
-  // 메뉴 클릭 이벤트 설정
+  // 각 메뉴 클릭 시 이벤트 추가
   menuLinks.forEach(link => {
     link.addEventListener('click', (event) => {
-      event.preventDefault();
+      event.preventDefault(); // 기본 동작 방지
 
-      // 모든 링크에서 active 클래스 제거
+      // 모든 링크의 active 클래스 제거
       menuLinks.forEach(link => link.classList.remove('active'));
-      
+
       // 클릭한 링크에 active 클래스 추가
       link.classList.add('active');
 
       // 모든 섹션 숨기기
       sections.forEach(section => section.classList.remove('active'));
 
-      // 클릭한 링크에 연결된 섹션 표시
+      // 클릭한 메뉴와 연결된 섹션 표시
       const targetSection = document.getElementById(link.dataset.section);
       targetSection.classList.add('active');
     });
