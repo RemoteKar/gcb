59b9165a839f489128d17b8a1beac3a8a0ede687
Dongkyun An <79748558+eiqq@users.noreply.github.com>
2025-02-11 18:47:15 -0800

    ss

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", () => {
   searchButton.addEventListener("click", () => {
     const nickname = nicknameInput.value.trim();
     if (!nickname) {
-      resultDisplay.textContent = "닉네임을 입력하세요.";
       return;
     }
     // 검색 후 /user/{닉네임}으로 이동
--- client/scripts/user.js
+++ client/scripts/user.js
@@ -2,12 +2,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   const searchButton = document.getElementById("search-button");
   const nicknameInput = document.getElementById("nickname");
-  const resultDisplay = document.getElementById("result");
-
   searchButton.addEventListener("click", () => {
     const nickname = nicknameInput.value.trim();
     if (!nickname) {
-      resultDisplay.textContent = "닉네임을 입력하세요.";
       return;
     }
     // 검색 후 /user/{닉네임}으로 이동
