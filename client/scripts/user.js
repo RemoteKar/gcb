1ba14ff9b3471bc9eae0804e079a01fade055954
Dongkyun An <79748558+eiqq@users.noreply.github.com>
2025-04-08 20:35:53 -0700

    asdf

--- client/scripts/user.js
+++ client/scripts/user.js
@@ -210,7 +210,7 @@ function renderNextGames(uuid) {
     const cardBg = (ranking === 1) ? "#4066B2" : "#2c2c2c";
     const cardBorder = (ranking === 1) ? "#5383E8" : "#3c3c3c";
     gameItem.innerHTML = `
-      <div class="game-card" style="background-color: ${cardBg}; border-color: ${cardBorder};">
+    <div class="game-card" style="background-color: ${cardBg}; border-color: ${cardBorder};">
         <div class="game-card-left">
           <img src="/Resource/character/${playerData.Character}.png" alt="캐릭터">
         </div>
