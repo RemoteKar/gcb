7c76434c7f809b8551c53bee3e10a967ce8afd63
DAN <dongkyunan0928@gmail.com>
2025-07-02 01:29:55 -0700

    gemini

--- client/scripts/user.js
+++ client/scripts/user.js
@@ -267,4 +267,3 @@ function formatUUID(uuid) {
   return `${uuid.slice(0, 8)}-${uuid.slice(8, 12)}-${uuid.slice(12, 16)}-${uuid.slice(16, 20)}-${uuid.slice(20)}`;
 }
 
-module.exports = { formatUUID };
