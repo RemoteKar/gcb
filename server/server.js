e70e717c6503395608273c92da82d90efc4c295e
DAN <dongkyunan0928@gmail.com>
2025-07-02 03:57:27 -0700

    gemini

--- server/server.js
+++ server/server.js
@@ -16,9 +16,7 @@ async function initializeRankings() {
 // 서버리스 핸들러 정의
 module.exports.handler = async (event, context) => {
   // 랭킹 데이터가 초기화되지 않았다면 초기화
-  if (apiRoutes.precalculatedLeaderboard.length === 0) {
-    await initializeRankings();
-  }
+  await initializeRankings();
   return serverless(app)(event, context);
 };
 
